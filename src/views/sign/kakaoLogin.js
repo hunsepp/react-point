2a50289be37de716263bcb0251a4ec11c12d26bc
chueunjeong <miho7724@gmail.com>
2020-09-02 23:09:40 +0900

    [chu]add login card

--- src/views/sign/kakaoLogin.js
+++ src/views/sign/kakaoLogin.js
@@ -1,49 +1,57 @@
 import KakaoLogin from "react-kakao-login";
+import { browserHistory } from "react-router";
 import React, { Component, useState } from "react";
 import axios from "axios";
+const dotenv = require("dotenv");
 
 const KLogin = () => {
   const [id, setId] = useState([]);
   const [nickname, setNickname] = useState([]);
   const [thumbnail_image, setThumbnail_image] = useState([]);
+  const [access_token, setAccessToken] = useState([]);
+  const [refresh_token, setRefreshToken] = useState([]);
+  const [kuser, setKuser] = useState();
 
   //카카오 로그인 성공시
-  responseKakao = res => {
+  const responseKakao = (res) => {
     setId(res.profile.id);
     setNickname(res.profile.properties.nickname);
     setThumbnail_image(res.profile.properties.thumbnail_image);
+    setAccessToken(res.response.access_token);
+    setRefreshToken(res.response.refresh_token);
 
     axios
-      .get("/api/kakao/signUp", {
-        params: {
-          id,
-          nickname,
-          thumbnail_image
-        }
+      .post("/api/kakao/signUp", {
+        user_id: id,
+        user_nickname: nickname,
+        user_thumbnail_image: thumbnail_image,
+        user_access_token: access_token,
+        user_refresh_token: refresh_token,
       })
-      .then(result => {
-        console.log(result.data);
-        if (result.data != null) {
-          //resultIsVisible = true;
-          //barIsVisible = false;
+      .then(({ data }) => {
+        if (data.checkSaving == 1) {
+          localStorage.setItem("userAccessToken", access_token);
+          setKuser(data.kuser);
+          window.location = "/blog-overview";
         }
       });
   };
 
-  responseFail = err => {
-    console.error(err);
+  const responseFail = (err) => {
+    alert(err);
   };
   return (
     <div>
       <KakaoLogin
         //카카오에서 발급받은 API KEY값
-        jsKey="89c4c146837a62268d96281c80662bdd"
+        jsKey={"89c4c146837a62268d96281c80662bdd"}
         //로그인 성공시 실행할 함수
-        onSuccess={this.responseKakao}
+        onSuccess={responseKakao}
         //로그인 실패시 실행할 함수
-        onFailure={this.responseFail}
+        onFailure={responseFail}
         //사용자 프로필 정보 가져오기
         getProfile={true}
+        useDefaultStyle
       />
     </div>
   );
--- src/views/sign/login.js
+++ src/views/sign/login.js
@@ -1,122 +1,39 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import KLogin from "./KakaoLogin";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-}));
-
+import { Card, CardHeader, CardBody, CardFooter } from "shards-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
 export default function SignIn() {
-  const classes = useStyles();
-
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <br />
-          {/* 카카오톡 로그인 시작 */}
-          <KLogin />
-          {/* 카카오톡 로그인 끝 */}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+    <Card
+      style={{
+        textAlign: "center",
+        position: "absolute",
+        top: "300%",
+
+        left: "45%",
+      }}
+    >
+      <div>
+        <br />
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+      <CardHeader
+        style={{
+          color: "#ffe812",
+          fontSize: "large",
+        }}
+      >
+        <FontAwesomeIcon icon={faCog} size="3x" spin />
+        <br />
+        SIGN IN
+        <br />
+      </CardHeader>
+      <CardBody>
+        {/* 카카오톡 로그인 시작 */}
+        <KLogin />
+        {/* 카카오톡 로그인 끝 */}
+      </CardBody>
+      <CardFooter>@kunité</CardFooter>
+    </Card>
   );
 }
