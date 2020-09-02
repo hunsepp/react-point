a4d1123c7cb7ffb8770d147c6636fa3f16e2bf21
hunhand <64964615+hunsepp@users.noreply.github.com>
2020-09-03 00:11:24 +0900

    Add files via upload
    [park]store

---
+++ src/myCoupon.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+    Container,
+    Row,
+    Col,
+    Card,
+    CardBody,
+    CardFooter,
+    Badge,
+    Button
+  } from "shards-react";
+  
+import PageTitle from "../components/common/PageTitle";
+
+export default function MyStore(){
+    const PostsListOne = [
+        {
+          backgroundImage: require("../images/content-management/1.jpeg"),
+          storeName:"카페1"
+        }
+      ]
+      
+    return (
+        
+        <Container fluid className="main-content-container px-4">
+        {/* Page Header */}
+        <Row noGutters className="page-header py-4">
+            {PostsListOne.map((post, idt) => (
+            <PageTitle key = {idt} sm="4" title={post.storeName} className="text-sm-left" />
+            ))}
+        </Row>
+
+        {/* First Row of Posts */}
+        <Row>
+        {PostsListOne.map((post, idt) => (
+            <Col lg="6" md="10" sm="10" className="mb-4" key = {idt}>
+              <Card small className="card-post card-post--1">
+                <div
+                  className="card-post__image"
+                  style={{ backgroundImage: `url('${post.backgroundImage}')` }}
+                >
+                </div>
+                
+              </Card>
+            </Col>
+        ))}
+        </Row>
+        
+        </Container>
+        
+        
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,14 @@ import Errors from "./views/Errors";
 import ComponentsOverview from "./views/ComponentsOverview";
 import Tables from "./views/Tables";
 import BlogPosts from "./views/BlogPosts";
+//여기부터 우리가 추가한 거
 import KAS from "./views/KAS"
+import Login from "./views/sign/login"
+import Check from "./views/sign/check"
+import Menu from "./views/Menu";
+import Store from "./views/Store";
+import myStore from "./views/myStore";
+import myCoupon from "./views/myCoupon";
 
 export default [
   {
@@ -61,5 +68,34 @@ export default [
     layout: DefaultLayout,
     component: KAS
   },
-  
+  {
+    path: "/login",
+    layout: DefaultLayout,
+    component: Login
+  },
+  {
+    path: "/check",
+    layout: DefaultLayout,
+    component: Check
+  },
+  {
+    path: "/menu",
+    layout: DefaultLayout,
+    component: Menu
+  },
+  {
+    path: "/store",
+    layout: DefaultLayout,
+    component: Store
+  },
+  {
+    path: "/mystore",
+    layout: DefaultLayout,
+    component: myStore
+  },
+  {
+    path: "/mycoupon",
+    layout: DefaultLayout,
+    component: myCoupon
+  }
 ];
