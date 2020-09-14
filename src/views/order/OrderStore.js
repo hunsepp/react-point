ac07b1d7fb5e6aa70e6623dc080063b3b23f58ed
jumpupmoon <jumpupmoon@naver.com>
2020-09-15 06:03:17 +0900

    [hwang] 주문하기 매장 위치 지도에 마커로 표시 / 마커 선택시 해당 매장 주문하기로 이동

--- src/views/order/OrderStore.js
+++ src/views/order/OrderStore.js
@@ -5,6 +5,7 @@ import PageTitle from '../../components/common/PageTitle';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchLocation } from "@fortawesome/free-solid-svg-icons";
 import Loader from '../Loader';
+const {kakao} = window;
 
 export default function OrderStore({history}){
   const [stores, setStores] = useState([]);
@@ -19,10 +20,43 @@ export default function OrderStore({history}){
   useEffect(() => {
     axios.get('/api/store')
     .then(({data}) => {
-      setStores(data.stores);
       setLoading(false);
+      setStores(data.stores);
     })
   }, []);
+
+  useEffect(() => {
+    if(loading) return;
+    
+    // 지도 그리기
+    const container = document.getElementById("map");
+    const options = {
+        center: new kakao.maps.LatLng('37.4873275895417', '127.014465676374'),
+        level: 7,
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    // 마커 추가하기
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    console.log(stores);
+    stores.map(store => {
+      geocoder.addressSearch(store.address, (res, status) => {
+        if(status === "OK") {
+          // 해당 위치 마커로 표시
+          const marker = new kakao.maps.Marker({
+              position: new kakao.maps.LatLng(res[0].y, res[0].x),
+              map: map,
+          });
+
+          // 마커 클릭시 주문 화면으로
+          kakao.maps.event.addListener(marker, 'click', function() {
+            history.push(`/ordermenu?id=${store._id}`);
+          });
+        }
+      })
+    })
+  }, [stores])
     
   return loading ? <Loader loading={loading} /> : (
     <Container fluid className="main-content-container px-4">
@@ -31,34 +65,41 @@ export default function OrderStore({history}){
       </Row>
 
       <Row>
-        {stores.map((store, idx) => (
-          <Col lg="3" md="6" sm="12" className="mb-4" key={idx} onClick={() => moveMenu(store._id)}>
-            <Card small className="card-post card-post--1">
-              <div className="card-post__image" style={{ backgroundImage: `url(/api/upload/${store.account})` }}>
-                <Badge pill className={`card-post__category bg-dark`}
-                >{store.category}</Badge>
-              </div>
-              <CardBody>
-                <Row>
-                <Col sm="8">
-                <h5 className="card-title">{store.name}</h5>
-                </Col>
-                <Col sm="4">
-                <FontAwesomeIcon icon={faSearchLocation} color="black" size="2x" />
-                </Col>
-                </Row>
-                <p className="card-text d-block mt-3">{store.address}</p>
-                
-                <p className="card-text d-block">
-                  영업시간
-                  {store.open && store.close &&
-                    <span>　　{store.open} ~ {store.close}</span>
-                  }
-                </p>
-              </CardBody>
-            </Card>
-          </Col>
-        ))}
+        <Col id="map" lg="6" sm="12" style={{height: '300px'}} className="mb-5">
+        </Col>
+
+        <Col lg="6" sm="12">
+          <Row>
+            {stores.map((store, idx) => (
+              <Col sm="6" className="mb-4" key={idx} onClick={() => moveMenu(store._id)}>
+                <Card small className="card-post card-post--1">
+                  <div className="card-post__image" style={{ backgroundImage: `url(/api/upload/${store.account})` }}>
+                    <Badge pill className={`card-post__category bg-dark`}
+                    >{store.category}</Badge>
+                  </div>
+                  <CardBody>
+                    <Row>
+                    <Col sm="8">
+                    <h5 className="card-title">{store.name}</h5>
+                    </Col>
+                    <Col sm="4">
+                    <FontAwesomeIcon icon={faSearchLocation} color="black" size="2x" />
+                    </Col>
+                    </Row>
+                    <p className="card-text d-block mt-3">{store.address}</p>
+                    
+                    <p className="card-text d-block">
+                      영업시간
+                      {store.open && store.close &&
+                        <span>　　{store.open} ~ {store.close}</span>
+                      }
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
       </Row>
     </Container>
   )
